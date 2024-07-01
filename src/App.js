699fe2cbc7fe3a9a83d0dcca1c69379d0ad044fd
q8wahab <q8_xprt@hotmail.com>
2024-07-01 20:22:13 +0300

    finish the register modal page that go to backend

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import NavBar from "./components/NavBar";
 import Announcement from "./components/Announcement";
+import { useEffect, useState } from "react";
+import { checkToken } from "./api/auth";
+import UserContext from "./Context/userContext";
 
 function App() {
+  const [user, setUser] = useState(false);
+
+  useEffect(() => {
+    setUser(checkToken());
+  }, []);
+
   return (
-    <div className="h-screen flex flex-col">
-      <Announcement />
-      <NavBar />
-      <Routes>
-        <Route path="/" Component={LandingPage} />
-      </Routes>
-    </div>
+    <UserContext.Provider value={[user, setUser]}>
+      <div className="h-screen flex flex-col">
+        <Announcement />
+        <NavBar />
+        <Routes>
+          <Route path="/" Component={LandingPage} />
+        </Routes>
+      </div>
+    </UserContext.Provider>
   );
 }
 
